566d722ee031102556b051b6f50a9d9ad79a5696
dubasevaOksana883 <dubasevaoksana883@gmail.com>
2019-01-04 04:13:18 +0300

    first

---
+++ js/index.js
@@ -0,0 +1,34 @@
+"use strict"
+class Game {
+  constructor(){
+    var createTags = (tagName, container = document.body) => {
+      var elem = container.appendChild (document.createElement(tagName))
+      return elem
+    }
+    this.mainCont = createTags ("div")
+    this.mainCont.className = "mainGround"
+    var parts = [
+      {
+        name: "castle",
+        background: "images/castle.png"
+      }
+    ]
+    this.scenarioGame = name => {
+      var curentPart = parts.filter( elem => elem.name === name)[0]
+      this.mainCont.style.backgroundImage = `url(${curentPart.background})`
+    }
+    this.start = () => {
+      var btn = createTags("div", this.mainCont)
+      btn.className = "startBtn"
+      btn.onclick = function (event){
+        console.warn( "gamestart")
+      }
+    }
+  }
+}
+var game = new Game()
+// var test = game.mainCont.appendChild(document.createElement("rec-form"))
+game.scenarioGame("castle")
+// test.assignment (game.start)
+// test.start()
+game.start()
---
+++ js/regForm.js
@@ -0,0 +1,201 @@
+"use strict"
+let script = document.createElement( 'script' )
+script.src = 'https://cdn.rawgit.com/chrisveness/crypto/4e93a4d/sha256.js'
+document.head.appendChild( script )
+
+class RegistrationForm extends HTMLElement{
+    constructor () {
+        super ()
+        var nextFunc
+        var addTag = (nameTag, cont = document.body)=> {
+            var elem = cont.appendChild(
+                document.createElement (nameTag)
+            )
+            return elem
+        }
+        var shadow = this.attachShadow({mode:"open"})
+        var mainCont = addTag ("div",shadow )
+        console.log(this)
+        var buttons = [
+            {
+                tagName: "div",
+                attrs: {
+                    innerText: "Вход",
+//                     className: "",
+                    onclick: event => {
+                           this.startEnter()
+                    }
+                }
+            },
+            {
+                tagName: "div",
+                attrs: {
+                    innerText: "Регистрация",
+//                     className: "",
+                    onclick: event => {
+                            this.startReg()
+                    }
+                }
+            },
+        ]
+        var createTags = (arr) => {
+            arr.forEach (
+                function (currentValue){
+                     if(!currentValue) return
+                     var elem = addTag (currentValue.tagName, mainCont)
+                     for (var attr in currentValue.attrs){
+                         elem[attr] = currentValue.attrs[attr]
+                     }
+                }
+            )
+        }
+
+        this.start = () => {
+            mainCont.innerHTML = ""
+            createTags(buttons)
+        }
+        this.startReg = () => {
+            mainCont.innerHTML = ""
+            createTags(registData)
+        }
+        this.startEnter = () => {
+            mainCont.innerHTML = ""
+            console.log(enterData)
+            createTags(enterData)
+        }
+        this.assignment = func => nextFunc = func
+        var registData = [
+            {
+                tagName: "h1",
+                attrs: {
+                    innerText: "Регистрация"
+                    // className: ""
+                }
+
+            },
+            {
+                tagName: "h3",
+                attrs: {
+                    innerText: "Введите имя"
+                    // className: ""
+                }
+            },
+            {
+                tagName: "input",
+                attrs: {
+                    id: "lg"
+                    //className: ""
+                }
+            },
+            {
+                tagName: "h3",
+                attrs: {
+                    innerText: "Адрес электроной почты",
+                    //className: ""
+                }
+            },
+            {
+                tagName: "input",
+                attrs: {
+                    type: "email",
+                    id: "em"
+                    //className: ""
+                }
+            },
+            {
+                tagName: "h3",
+                attrs: {
+                    innerText: "Введите пароль",
+                    //className:
+                }
+            },
+            {
+                tagName: "input",
+                attrs: {
+                    type: "password",
+                    id: "ps"
+                    //className: ""
+                }
+            },
+            {
+                tagName: "div",
+                attrs: {
+                    innerText: "Продолжить",
+                    //className:
+                    onclick: event => {
+                            console.log("start")
+                            var inp = Array.from(mainCont.children)
+                            .filter (
+                                elem => elem.tagName === "INPUT"
+                            )
+                            var prop = inp.every( x => x.value )
+                            console.log(prop)
+                            if (prop){
+                                    console.log ("if")
+                                    var obj = {}
+                                    inp.forEach (el => {
+                                            Object.assign(obj, {[el.id]: Sha256.hash(el.value)})
+                                    })
+                                        if ( inp.some ( el => el.id === "em" )){
+                                            localStorage.setItem("user", JSON.stringify(obj))
+                                                 this.end(nextFunc)
+                                        } else{
+                                            console.log("ent")
+                                            var checkUser = localStorage.getItem("user") ? JSON.parse(localStorage.getItem("user")):
+                                              {ps: null, lg: null}
+                                            if ( checkUser.ps === obj.ps && checkUser.lg === obj.lg){
+                                                 this.end(nextFunc)
+                                            }else {
+                                                  massageError("Такого пользователя нет")
+                                                  addBtn()
+                                            }
+                                        }
+                            }else massageError("Заполните поля*")
+                    }
+                }
+            }
+        ]
+        var enterData = registData.map ((elem, ind)=>{
+            var nElem = JSON.parse(JSON.stringify(elem))
+            if (ind === 0){
+                nElem.attrs.innerText = "Вход"
+                return nElem
+            }
+            if(elem.attrs.onclick){
+                nElem.attrs.onclick = elem.attrs.onclick
+            }
+            if (ind < 3 || ind > 4) return nElem
+        })
+        var massageStyle = "style='position:absolute; bottom:5px; color: red;'"
+        var addBtn = () => {
+          checkId("btn")
+          var btn = addTag("div", mainCont)
+          btn.innerText = "Вернуться назад"
+          btn.id = "btn"
+          btn.onclick = event => {
+            this.start()
+          }
+        }
+        var checkId = nid => {
+          Array.from(mainCont.children).forEach(el =>{
+            el.id === nid ? el.remove() : null
+          })
+        }
+        var massageError = text => {
+          checkId("error")
+          var mas = addTag("div", mainCont)
+          mas.id = "error"
+          mas.innerText = text
+          mas.style = `
+            color: red;
+            position: absolute;
+            bottom: 10px;
+          `
+        }
+        this.end = func => {
+          this.remove()
+          typeof func === "function" ? func () : null
+        }
+    }
+}
+customElements.define ("rec-form", RegistrationForm)
